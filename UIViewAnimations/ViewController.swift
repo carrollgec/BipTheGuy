c6d8189ef2c12e115f655919e94a62efc885f0f8
Grace <carrolhn@bc.edu>
2021-02-21 15:02:34 -0500

    Completed 4.2

--- UIViewAnimations/ViewController.swift
+++ UIViewAnimations/ViewController.swift
@@ -9,11 +9,56 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var moveButton: UIButton!
+    @IBOutlet weak var alphaButton: UIButton!
+    @IBOutlet weak var pulseButton: UIButton!
+    @IBOutlet weak var colorChangeButton: UIButton!
+    
+    var originalX: CGFloat!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        print("viewWillAppear imageView.frame.origin.x = \(imageView.frame.origin.x )")
+    }
+    
+    override func viewDidLayoutSubviews() {
+//        let deviceWidth = view.frame.width
+//        imageView.frame.origin.x = deviceWidth
+//        alphaButton.frame.origin.x = deviceWidth
+//        pulseButton.frame.origin.x = deviceWidth
+//        colorChangeButton.frame.origin.x = deviceWidth
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        print("viewDidAppear imageView.frame.origin.x = \(imageView.frame.origin.x )")
+        let deviceWidth = view.frame.width
+        imageView.frame.origin.x = deviceWidth
+        alphaButton.frame.origin.x = deviceWidth
+        pulseButton.frame.origin.x = deviceWidth
+        colorChangeButton.frame.origin.x = deviceWidth
+    }
+     
+    @IBAction func movePressed(_ sender: UIButton) {
+        let centerX = view.center.x
+        imageView.center.x = centerX
+        alphaButton.center.x = centerX
+        pulseButton.center.x = centerX
+        colorChangeButton.center.x = centerX
+    }
+    @IBAction func alphaPressed(_ sender: UIButton) {
+    }
+    @IBAction func pulsePressed(_ sender: UIButton) {
+    }
+    @IBAction func colorChangePressed
+    (_ sender: UIButton) {
+    }
+    
 
 }
 
